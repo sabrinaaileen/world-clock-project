1a47e5550edfd233cc35759d90398c8f74835d89
sabrinaaileen <115700137+sabrinaaileen@users.noreply.github.com>
2023-07-12 14:12:45 +0200

    Worked on select element with javascript

--- javascript/index.js
+++ javascript/index.js
@@ -30,5 +30,29 @@ function changeTime() {
     "HH:mm:ss [<small>] A [</small>]"
   );
 }
+
+function showSelectedCity(event) {
+  let cityTimezoneElement = event.target.value;
+  let cityName = cityTimezoneElement.replace("_", " ").split("/")[1];
+  let cityTimeElement = moment().tz(cityTimezoneElement);
+  let cityElement = document.querySelector("#selected-city");
+  cityElement.innerHTML = `
+<div class="cities">
+          <div>
+            <h2>${cityName}</h2>
+            <p class="date">${cityTimeElement.format("MMMM Do YYYY")}</p>
+          </div>
+          <div>
+            <h3 class="time">${cityTimeElement.format(
+              "HH:mm:ss [<small>] A [</small>]"
+            )}</h3>
+          </div>
+        </div>
+`;
+}
+
 changeTime();
 setInterval(changeTime, 1000);
+
+let selectedCity = document.querySelector("#select-city");
+selectedCity.addEventListener("change", showSelectedCity);
